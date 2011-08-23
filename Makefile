46f930040c638b1ab1c333bc5c292208793677a0
Dietmar Maurer <dietmar@proxmox.com>
2011-08-23 07:35:02 +0200

    imported from svn 'pve-jslint/trunk'

---
+++ Makefile
@@ -0,0 +1,61 @@
+RELEASE=2.0
+
+PACKAGE=pve-jslint
+VERSION=1.0
+PACKAGERELEASE=2
+
+PKGREL=${VERSION}-${PACKAGERELEASE}
+DEB=${PACKAGE}_${VERSION}-${PKGREL}_all.deb
+
+all: ${DEB}
+
+.PHONY: dinstall
+dinstall: ${DEB}
+	dpkg -i ${DEB}
+
+.PHONY: ${DEB}
+${DEB} deb:
+	make clean
+	rm -rf dest
+	mkdir dest
+	make DESTDIR=`pwd`/dest install
+	mkdir dest/DEBIAN
+	sed -e 's/@PKGREL@/${PKGREL}/' <control.in >dest/DEBIAN/control
+	mkdir -p dest/usr/share/doc/${PACKAGE}
+	install -m 0644 copyright dest/usr/share/doc/${PACKAGE}
+	install -m 0644 changelog.Debian dest/usr/share/doc/${PACKAGE}
+	gzip --best dest/usr/share/doc/${PACKAGE}/changelog.Debian
+	dpkg-deb --build dest
+	mv dest.deb ${DEB}
+	rm -rf dest
+	lintian ${DEB}	
+
+rhinoed_jslint.js: jslint.js rhino.js
+	cat jslint.js rhino.js >$@.tmp
+	mv $@.tmp $@
+
+install: rhinoed_jslint.js jslint
+	mkdir -p ${DESTDIR}/usr/share/${PACKAGE}
+	install -m 0644 rhinoed_jslint.js ${DESTDIR}/usr/share/${PACKAGE}/rhinoed_jslint.js
+	mkdir -p ${DESTDIR}/usr/bin
+	install -m 0755 jslint /usr/bin
+
+jslint.js download:
+	wget -O jslint.js http://jslint.com/jslint.js
+
+.PHONY: distclean
+distclean: clean
+
+.PHONY: clean
+clean:
+	rm -rf *~ dest control rhinoed_jslint.js *.deb
+
+.PHONY: upload
+upload: ${DEB}
+	umount /pve/${RELEASE}; mount /pve/${RELEASE} -o rw 
+	mkdir -p /pve/${RELEASE}/extra
+	rm -f /pve/${RELEASE}/extra/${PACKAGE}_*.deb
+	rm -f /pve/${RELEASE}/extra/Packages*
+	cp ${DEB} /pve/${RELEASE}/extra
+	cd /pve/${RELEASE}/extra; dpkg-scanpackages . /dev/null > Packages; gzip -9c Packages > Packages.gz
+	umount /pve/${RELEASE}; mount /pve/${RELEASE} -o ro
---
+++ rhino.js
@@ -0,0 +1,52 @@
+(function (a) {
+    var e, i, input, filename, defaults;
+    if (!a[0]) {
+        print("Usage: jslint.js file.js ...");
+        quit(1);
+    }
+
+    defaults = {
+	predef: ['Ext', 'PVE', 'PVE_vnc_console_event'],
+	devel:      true,
+	'continue': true,   /// Allow continue statement
+	bitwise:    true,   //  Allow bitwise operators
+        browser:    true,   //  Assume a browser 
+        css:        true,   //  Tolerate CSS workarounds
+        eqeq:       true,   //  Allow `==` && `!=`
+        //immed:      true,  //  Immediate invocations must be wrapped in parens.
+        //nomen:      true,  //  Allow dangling `_` in identifiers
+        newcap:     false,   //  Require initial caps for constructors
+        vars:       true,   //  Allow multiple `var` statements.
+        plusplus:   true,   //  Allow `++` and `--`
+        regexp:     true,  //   Allow `.` and `[^...]` in regex
+        sloppy:     true,   //  Don't require `use strict;`
+        undef:      false,  //  Disallow undeclared variables 
+        white:      true    //  Don't apply strict whitespace rules
+    };
+    
+    for (i = 0; i < a.length; ++i) {
+    	filename = a[i];
+	input = readFile( filename );
+	if (!input) {
+            print("jslint: Couldn't open file '" + filename + "'.");
+            quit(1);
+	}
+
+	if (!JSLINT(input, defaults)) {
+            for (i = 0; i < JSLINT.errors.length; i += 1) {
+		e = JSLINT.errors[i];
+		if (e) {
+                    print('[' + filename + '] Lint at line ' + e.line + ' character ' +
+                          e.character + ': ' + e.reason);
+                    print((e.evidence || '').
+                          replace(/^\s*(\S*(\s+\S+)*)\s*$/, "$1"));
+                    print('');
+		}
+            }
+            quit(2);
+	} else {
+            print("jslint: " + filename + " OK");
+	}
+    }
+    quit();
+}(arguments));
