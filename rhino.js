1c1609d473f97288c45c50c058b79453f63e2de0
Dietmar Maurer <dietmar@proxmox.com>
2011-11-21 13:01:35 +0100

    add 'gettext' to predefs

--- rhino.js
+++ rhino.js
@@ -6,7 +6,7 @@
     }
 
     defaults = {
-	predef: ['Ext', 'PVE', 'PVE_vnc_console_event', 'FormData'],
+	predef: ['Ext', 'PVE', 'PVE_vnc_console_event', 'FormData', 'gettext'],
 	devel:      true,
 	'continue': true,   /// Allow continue statement
 	bitwise:    true,   //  Allow bitwise operators
